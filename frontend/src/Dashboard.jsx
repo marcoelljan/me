dc4bcbdd824a439cb3c2a8ad93406ee6b5960af0
marcoelljan <vincentsmiley29@gmail.com>
2025-04-03 20:49:34 +0800

    Update .gitignore in frontend

--- frontend/src/Dashboard.jsx
+++ frontend/src/Dashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {Container, Typography, Button} from "@mui/material";
+import {Container, Typography, Button, Box} from "@mui/material";
 
 
 
@@ -24,10 +24,47 @@ useEffect(() => {
 return(
 
     <Container>
-<Typography variant="h4">Welcome {username}</Typography>
+      
+      
+        <Box 
+    
+        sx= {{
+            textAlign: "center",
+            p: 3,
+            mb: -5,
+            borderRadius: 2,
+            boxShadow: 3,
+            backgroundColor: "#f0f0f0",
+        }} > </Box>
+
+        
+<Typography variant="h4" sx ={{ color: "#3f51b5, mb:2"}}>Welcome {username}</Typography>
+
+<Box
+sx = {{
+    textAlign: "center",
+    p: 1,
+    mt: 2,
+    width: 100,
+    margin: "0 auto",
+    borderRadius: -1,
+    boxShadow: 3,
+    backgroundColor: "#e8f5e9",
+}}
+
+
+
+>
+
 <Button
  variant ="contained"
- color = "secondary"
+ sx={{ backgroundColor: "#4caf50",
+    color: "white",
+    "&:hover": {
+        backgroundColor: "#45a049",
+    },
+ }}
+ 
  onClick={() => {
 localStorage.removeItem("username");
 localStorage.removeItem("token");
@@ -37,6 +74,7 @@ navigate("/login");
 > 
 Logout
 </Button>
+</Box>
     </Container>
 );
 
